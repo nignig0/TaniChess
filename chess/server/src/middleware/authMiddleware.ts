9c57bff8c55093be082156c3ebadd7731e222aab
nignig0 <tanitoluwaadebayo@gmail.com>
2024-12-17 13:16:49 +0000

    a fix

--- chess/server/src/middleware/authMiddleware.ts
+++ chess/server/src/middleware/authMiddleware.ts
@@ -12,7 +12,7 @@ const handleToken = async(req: Request, res: Response, next: NextFunction)=>{
         const token = authHeader.split(' ')[1];
         const decoded = await TokenService.verifyToken(token);
         console.log('The decoded -> ', decoded);
-        req.user = decoded;
+        req.user = JSON.parse(decoded!.toString());
     }
     
     // if(!decoded){
